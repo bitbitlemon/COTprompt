a44e2ae634ef66e9e270a918f293435e26ad3d17
bitlemon <2991849028@qq.com>
2025-12-30 16:29:50 +0800

    1

--- 100n_run.sh
+++ 100n_run.sh
@@ -1,1 +1,1 @@
-python train.py   --seed 1 --trainer NLPrompt   --dataset-config-file configs/datasets/cifar100n.yaml   --config-file configs/trainers/NLPrompt/rn50.yaml   --output-dir output/cifar100n_safe_smoke_seed1   DATASET.NUM_SHOTS 16 DATASET.USE_OT True OPTIM.MAX_EPOCH 2 TRAIN.CHECKPOINT_FREQ 0
+python train.py --seed 1 --trainer NLPrompt --dataset-config-file configs/datasets/cifar100n.yaml --config-file configs/trainers/NLPrompt/rn50.yaml --output-dir output/cifar100n_debug_fix DATASET.NUM_SHOTS -1 DATASET.USE_OT True OPTIM.MAX_EPOCH 20 TRAIN.CHECKPOINT_FREQ 10
