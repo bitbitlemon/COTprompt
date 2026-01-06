11f693deb835595364434965c18f7015180930e0
Qining Luo <136308066+bitbitlemon@users.noreply.github.com>
2026-01-06 17:16:59 +0800

    Update 100n_run.sh

--- 100n_run.sh
+++ 100n_run.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env bash
-
 python train.py \
   --seed 1 \
   --trainer NLPrompt \
   --dataset-config-file configs/datasets/cifar100n.yaml \
   --config-file configs/trainers/NLPrompt/rn50.yaml \
-  --output-dir output/cifar100n_nlprompt_full_noise_seed1 \
+  --output-dir output/cifar100n_nlprompt_cleancheck_seed1 \
   DATASET.NUM_SHOTS -1 \
-  DATASET.NOISE_LABEL True \
-  DATASET.NOISE_RATE 0.5 \
-  DATASET.NOISE_TYPE sym \
+  DATASET.NOISE_LABEL False \
   DATASET.USE_OT False \
   OPTIM.MAX_EPOCH 50 \
   TRAIN.CHECKPOINT_FREQ 10 \
