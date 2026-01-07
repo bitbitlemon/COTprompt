517e4f3a7c94a6c46c5387570cfac258a27afd32
Qining Luo <136308066+bitbitlemon@users.noreply.github.com>
2026-01-07 10:35:07 +0800

    Create best.sh

---
+++ best.sh
@@ -0,0 +1,13 @@
+#Acc:47%
+python train.py \
+  --seed 1 \
+  --trainer NLPrompt \
+  --dataset-config-file configs/datasets/cifar100n.yaml \
+  --config-file configs/trainers/NLPrompt/rn50.yaml \
+  --output-dir output/cifar100n_nlprompt_cleancheck_seed1 \
+  DATASET.NUM_SHOTS -1 \
+  DATASET.NOISE_LABEL False \
+  DATASET.USE_OT False \
+  OPTIM.MAX_EPOCH 50 \
+  TRAIN.CHECKPOINT_FREQ 10 \
+  TEST.NO_TEST False
