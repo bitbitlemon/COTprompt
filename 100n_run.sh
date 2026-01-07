8eefa70edae4f52b5ce2da07f4bb6749202c1f2b
Qining Luo <136308066+bitbitlemon@users.noreply.github.com>
2026-01-07 09:50:14 +0800

    Update training script for CIFAR100N dataset

--- 100n_run.sh
+++ 100n_run.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash
-
 python train.py \
   --seed 1 \
   --trainer NLPrompt \
   --dataset-config-file configs/datasets/cifar100n.yaml \
   --config-file configs/trainers/NLPrompt/rn50.yaml \
-  --output-dir output/cifar100n_nlprompt_full_noise_safeaug_seed1 \
+  --output-dir output/cifar100n_nlprompt_full_native_noise_seed1 \
   DATASET.NUM_SHOTS -1 \
   DATASET.NOISE_LABEL True \
-  DATASET.NOISE_RATE 0.5 \
-  DATASET.NOISE_TYPE sym \
+  DATASET.NOISE_RATE 0.0 \
   DATASET.USE_OT False \
-  INPUT.TRANSFORMS "('random_flip','normalize')" \
+  INPUT.CROP_PADDING 4 \
+  INPUT.TRANSFORMS "('random_crop','random_flip','normalize')" \
   OPTIM.LR 0.005 \
   OPTIM.MAX_EPOCH 100 \
   TRAIN.CHECKPOINT_FREQ 10 \
