a4fb8eb8a3345b01d99f6740f01f334f7f8c3606
Qining Luo <136308066+bitbitlemon@users.noreply.github.com>
2026-01-07 10:13:19 +0800

    Update 100n_run.sh

--- 100n_run.sh
+++ 100n_run.sh
@@ -3,16 +3,10 @@ python train.py \
   --trainer NLPrompt \
   --dataset-config-file configs/datasets/cifar100n.yaml \
   --config-file configs/trainers/NLPrompt/rn50.yaml \
-  --output-dir output/cifar100n_nlprompt_pureclip_seed1 \
+  --output-dir output/cifar100n_nlprompt_cleancheck_seed1 \
   DATASET.NUM_SHOTS -1 \
-  DATASET.NOISE_LABEL True \
-  DATASET.NOISE_RATE 0.5 \
-  DATASET.NOISE_TYPE sym \
+  DATASET.NOISE_LABEL False \
   DATASET.USE_OT False \
-  INPUT.CROP_PADDING 0 \
-  INPUT.TRANSFORMS "('random_resized_crop','random_flip','normalize')" \
-  INPUT.RRCROP_SCALE "(0.5,1.0)" \
-  OPTIM.LR 0.005 \
-  OPTIM.MAX_EPOCH 100 \
+  OPTIM.MAX_EPOCH 50 \
   TRAIN.CHECKPOINT_FREQ 10 \
   TEST.NO_TEST False
