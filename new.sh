9342283a65955961a88dd062c57181f1c5036e7d
bitlemon <2991849028@qq.com>
2026-01-08 09:38:28 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -13,17 +13,17 @@ python dividemix_cifar100n.py \
   --lambda-u 25.0 \
   --lambda-u-ramp 16
 
-# python dividemix_cifar100n.py \
-#   --root ./datasets/DATA \
-#   --output-dir ./output/cifar100n_dividemix_nlprompt \
-#   --arch-type nlprompt \
-#   --backbone RN50 \
-#   --epochs 200 \
-#   --warmup-epochs 10 \
-#   --batch-size 64 \
-#   --lr 0.005 \
-#   --p-threshold 0.5 \
-#   --T 0.5 \
-#   --alpha 4.0 \
-#   --lambda-u 25.0 \
-#   --lambda-u-ramp 16
+python dividemix_cifar100n.py \
+  --root ./datasets/DATA \
+  --output-dir ./output/cifar100n_dividemix_nlprompt \
+  --arch-type nlprompt \
+  --backbone RN50 \
+  --epochs 200 \
+  --warmup-epochs 10 \
+  --batch-size 64 \
+  --lr 0.005 \
+  --p-threshold 0.5 \
+  --T 0.5 \
+  --alpha 4.0 \
+  --lambda-u 25.0 \
+  --lambda-u-ramp 16
